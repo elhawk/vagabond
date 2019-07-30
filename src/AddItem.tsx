d37b3255b634e7c1f4a41e18116cfb7013bfa55c
Elizabeth Hawkins <hawkins.elh@gmail.com>
2019-07-29 22:01:45 -0700

    plumbed through handleInput on AddItem form changes

--- src/AddItem.tsx
+++ src/AddItem.tsx
@@ -24,23 +24,40 @@ export interface IAddItemProps {
 // open a form for the user to input values described in the passed in prop
 export class AddItem extends React.Component<IAddItemProps> {
     render() {
-        return <AddForm itemsToAdd={this.props.itemsToAdd} onSubmit= {() => {}}/>
+        return <AddForm 
+            itemsToAdd={this.props.itemsToAdd}
+            handleInput={this.handleInput}
+            onSubmit= {() => {}}/>
     }
 
+    handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+        let name: string = event.target.name;
+        let value: string = event.target.value.toString();
+        console.log(value); 
+    }
 }
 
 // todo: add state tracking stuff
-function createFormInput(item: IItemToAdd) {
+function createFormInput(item: IItemToAdd, handleInput: ((event: React.ChangeEvent<HTMLInputElement> ) => (void) )) {
     return (
-        // todo fix the keys
+        // todo: make this key guaranteed unique
         <p key={item.name}>
             {item.name}
-            <input type={item.type.toString()} required={item.required}></input>
+            <input 
+                type={item.type.toString()} 
+                required={item.required}
+                onChange={handleInput}></input>
         </p>);   
 }
 
-function AddForm(props: {itemsToAdd: IItemToAdd[], onSubmit: ()=> void}) {
-    let formInputs = props.itemsToAdd.map(createFormInput);
+function AddForm(props: {
+    itemsToAdd: IItemToAdd[],
+    handleInput: ((event: React.ChangeEvent<HTMLInputElement> ) => (void)),
+    onSubmit: ()=> void} ){
+    let formInputs = [];
+    for(let item of props.itemsToAdd) {
+        formInputs.push(createFormInput(item, props.handleInput));
+    }
     return (
         <form onSubmit = {props.onSubmit}>
             {formInputs}
